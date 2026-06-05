7dcd4427baa10488b5a73f29498c87f51539aca0
nqthtmlcssjs <nqt.tt.md.hn@gmail.com>
2026-06-05 16:57:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbzBpjLcr3weEaJQx9IsW3yrt3tvpea3mjgfv3gfDZXnEQS3dkrgEHcbIrx8qOa6yUF7/exec";
+
+function loadTasks() {
+  fetch(API_URL + "?t=" + Date.now())
+    .then(response => response.json())
+    .then(data => {
+      const tbody = document.getElementById("task-list");
+
+      if (!tbody) return;
+
+      tbody.innerHTML = "";
+
+      if (!data || data.length === 0) {
+        tbody.innerHTML = "<tr><td colspan='5'>Chưa có lịch công việc</td></tr>";
+        return;
+      }
+
+      data.forEach(item => {
+        tbody.innerHTML += `
+          <tr>
+            <td>${item.thoigian || ""}</td>
+            <td>${item.diadiem || ""}</td>
+            <td>${item.noidung || ""}</td>
+            <td>${item.hinhthucthamdu || ""}</td>
+            <td>${item.thanhphanthamdu || ""}</td>
+          </tr>
+        `;
+      });
+    })
+    .catch(error => {
+      const tbody = document.getElementById("task-list");
+
+      if (tbody) {
+        tbody.innerHTML = "<tr><td colspan='5'>Không tải được dữ liệu</td></tr>";
+      }
+
+      console.error(error);
+    });
+}
+
+loadTasks();
+
+setInterval(loadTasks, 5000);
